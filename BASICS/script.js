90f81222c67100192b25fb9e07c6b796454c7611
NicoSEAv <nico.buonfrate@gmail.com>
2025-01-28 23:47:00 +0100

    Switch Statement

--- BASICS/script.js
+++ BASICS/script.js
@@ -92,3 +92,31 @@ console.log("23" > "18"); // true
 // == performs type coercion, so a string is converted into number to make a comparison and obtain a boolean result.
 
 // === does not perform type coercion
+
+// ------------------------------------------------------------------------------------------------------------------------------------
+////////////////////////////////////
+// The switch Statement
+const day = "friday";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
